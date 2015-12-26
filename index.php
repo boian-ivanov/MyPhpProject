0c5b89c31b6ae76969cbac185aa81a4d4aef1e22
Boian Ivanov <boian.ivanov44@gmail.com>
2015-12-26 21:08:27 +0200

    v0.05
    Login form with kinda working database and begging of a register form

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
 <?php
+require('login.php');
 
+if(isset($_SESSION['login_user'])){
+	header("Location: profile.php");
+}
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div id="main">
+<h1>Login Session</h1>
+<div id="login">
+<h2>Login Form</h2>
+<form action="" method="post">
+<label>Username : </label>
+<input id="name" name="username" placeholder=" Type here" type="text">
+<label>Password : </label>
+<input id="password" name="password" placeholder=" Type here" type="password">
+<input name="submit" type="submit" value="Login">
+<span><?php echo $error; ?></span>
+</form>
+</div>
+</div>
+</body>
+</html>

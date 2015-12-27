6a671118caea04f9dac75f0498a04c7106c842e5
Boian Ivanov <boian.ivanov44@gmail.com>
2015-12-27 14:26:11 +0200

    v.0.07
    Semi-working register form

--- index.php
+++ index.php
@@ -15,13 +15,15 @@ if(isset($_SESSION['login_user'])){
 <div id="main">
 <h1>Login Session</h1>
 <div id="login">
-<h2>Login Form</h2>
+<h2>Login to your account.</h2>
 <form action="" method="post">
-<label>Username : </label>
-<input id="name" name="username" placeholder=" Type here" type="text">
-<label>Password : </label>
-<input id="password" name="password" placeholder=" Type here" type="password">
+<!--<label>Username : </label>-->
+<input id="name" name="username" placeholder="Username" type="text">
+<!--<label>Password : </label>-->
+<input id="password" name="password" placeholder="Password" type="password">
 <input name="submit" type="submit" value="Login">
+<b id="register"><a href="register.php">Register</a></b>
+<br>
 <span><?php echo $error; ?></span>
 </form>
 </div>
--- register.php
+++ register.php
@@ -1,4 +1,35 @@
 <?php 
 session_start();
+if(isset($_SESSION['user'])!=""){
+	header("Location: index.php");
+}
+require_once 'session.php';
 
+if(isset($_POST['btn_register'])){
+	$username = mysql_real_escape_string($_POST['username']);
+	$password = md5(mysql_real_escape_string($_POST['password']));
+}
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Register Form</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<center>
+<div id="main">
+<h1>Register Session</h1>
+<div id="login">
+<h2>Register your account.</h2>
+<form action="" method="post">
+<input id="name" name="username" placeholder="Username" type="text">
+<input id="password" name="password" placeholder="Password" type="password">
+<input name="submit" type="submit" value="Register">
+</form>
+</div>
+</div>
+</center>
+</body>
+</html>

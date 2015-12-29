730cd07a709d996a0b075e987884f3ed3ab2372e
Boian Ivanov <boian.ivanov44@gmail.com>
2015-12-29 17:42:36 +0200

    v.0.10
    Almost fully working register tab & fully integrated in database. +
    uploaded on a domain (http://deadneon.uchenici.bg/)

--- profile.php
+++ profile.php
@@ -4,12 +4,12 @@ include('session.php');
 <!DOCTYPE html>
 <html>
 <head>
-<title> Login Test (MyPhpProject)</title>
+<title><?php echo ucfirst($login_session);?>'s Profile</title>
 <link href="style.css" rel="stylesheet" type="text/css">
 </head>
 <body>
 <div id="profile">
-<b id="welcome">Welcome : <i><?php echo $login_session; ?></i></b>
+<b id="welcome">Welcome : <i><?php echo ucfirst($login_session); ?></i></b>
 <b id="logout"><a href="logout.php">Logout</a></b>
 </div>
 </body>
--- register.php
+++ register.php
@@ -1,13 +1,24 @@
 <?php 
 session_start();
-if(isset($_SESSION['user'])!=""){
+if(isset($_SESSION['login_user'])!=""){
 	header("Location: index.php");
 }
-require_once 'session.php';
+require_once 'dbconnect.php';
 
 if(isset($_POST['btn_register'])){
 	$username = mysql_real_escape_string($_POST['username']);
-	$password = md5(mysql_real_escape_string($_POST['password']));
+	$password = mysql_real_escape_string($_POST['password']);
+	
+	if(mysql_query("INSERT INTO 'myphpproject'.'login'('id','username','password') VALUES ('2','$username','$passsword')")){
+		?>
+			<script>alert('succesfully registered');</script>
+			<?php
+			header("Location: index.php");
+	} else {
+		?>
+			<script>alert('an error occured while registering you');</script>
+			<?php
+	}
 }
 ?>
 
@@ -18,6 +29,8 @@ if(isset($_POST['btn_register'])){
 <link href="style.css" rel="stylesheet" type="text/css">
 </head>
 <body>
+<b id="Home"><a href="index.php">Home</a></b>
+<br>
 <center>
 <div id="main">
 <h1>Register Session</h1>
@@ -26,7 +39,7 @@ if(isset($_POST['btn_register'])){
 <form action="" method="post">
 <input id="name" name="username" placeholder="Username" type="text">
 <input id="password" name="password" placeholder="Password" type="password">
-<input name="submit" type="submit" value="Register">
+<input id="btn_register" name="btn_register" type="submit" value="Register">
 </form>
 </div>
 </div>
